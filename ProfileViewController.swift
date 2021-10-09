5722ba8e93aa25dbb5073a30e1f24502d74015f9
pranavrav09 <71946217+pranavrav09@users.noreply.github.com>
2021-10-09 12:15:48 -0700

    Update ProfileViewController.swift

--- ProfileViewController.swift
+++ ProfileViewController.swift
@@ -8,42 +8,60 @@
 import UIKit
 import FirebaseDatabase
 import FirebaseAuth
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var tableView: UITableView!
-    var Prescription = ""
-    var ref: DatabaseReference?
-    var Categories = ["AM/PM", "Frequency", "Prescription Amount", "Starting Time"]
-    var values: [String] = []
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        ref = Database.database().reference()
-        ref?.child(Auth.auth().currentUser!.uid).child("Prescriptions").child(Prescription).observeSingleEvent(of: .value, with: { snapshot in
-            let value = snapshot.value as? NSDictionary
-            self.values.append(value?["AM_PM"] as? String ?? "")
-            self.values.append(value?["Frequency"] as? String ?? "")
-            self.values.append("\(value?["Prescription Amount"] as? Int ?? 0)")
-            self.values.append(value?["Starting_Time"] as? String ?? "")
-        })
-        
-        
-        setConstraints()
-        
-        print(Prescription)
-        // Do any additional setup after loading the view.
+        var Prescription = ""
+        var ref: DatabaseReference?
+        var Categories = ["AM/PM", "Frequency", "Prescription Amount", "Starting Time"]
+        var values: [String] = []
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            ref = Database.database().reference()
+            ref?.child(Auth.auth().currentUser!.uid).child("Prescriptions").child(Prescription).observeSingleEvent(of: .value, with: { snapshot in
+                let value = snapshot.value as? NSDictionary
+                self.values.append(value?["AM_PM"] as? String ?? "")
+                self.values.append(value?["Frequency"] as? String ?? "")
+                self.values.append("\(value?["Prescription Amount"] as? Int ?? 0)")
+                self.values.append(value?["Starting_Time"] as? String ?? "")
+            })
+            
+            
+            setConstraints()
+            
+            print(Prescription)
+            // Do any additional setup after loading the view.
+        }
+        func setConstraints(){
+            titleLabel.frame = CGRect(x: self.view.frame.width/4.0, y: self.view.frame.minY + 50, width: self.view.frame.width/2.0, height: titleLabel.frame.height)
+            tableView.frame = CGRect(x: self.view.frame.width/4.0, y: titleLabel.frame.maxY + 50, width: self.view.frame.width/2.0, height: self.view.frame.height/2.0)
+        }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-    func setConstraints(){
-        titleLabel.frame = CGRect(x: self.view.frame.width/4.0, y: self.view.frame.minY + 50, width: self.view.frame.width/2.0, height: titleLabel.frame.height)
-        tableView.frame = CGRect(x: self.view.frame.width/4.0, y: titleLabel.frame.maxY + 50, width: self.view.frame.width/2.0, height: self.view.frame.height/2.0)
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return values.count
+        
     }
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "Put ID here", for: indexPath)
+        cell.textLabel?.text = Categories[indexPath.row]
+        let label = cell.viewWithTag(1) as! UILabel
+        label.text = values[indexPath.row]
+        cell.addSubview(label)
+        return cell
+        
+    }
     /*
+     
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
+        // Pass the selected object to the new view controller.
     }
     */
 
